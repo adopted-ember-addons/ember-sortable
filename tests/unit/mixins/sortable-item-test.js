fe5cc44fc423bbebba91028b30c31b8928d6d18a
Jamie White <jamie@jgwhite.co.uk>
2015-04-26 15:00:16 +0100

    Add tests for getting/setting y

--- tests/unit/mixins/sortable-item-test.js
+++ tests/unit/mixins/sortable-item-test.js
@@ -1,38 +1,72 @@
 import Ember from 'ember';
 import SortableItemMixin from 'ember-sortable/mixins/sortable-item';
 import { module, test } from 'qunit';
+const { Component, run } = Ember;
 
-let Klass;
+const MockComponent = Component.extend(SortableItemMixin);
+
 let subject;
-let el;
 
 module('mixin:sortable-item', {
   beforeEach() {
-    el = $('<div />');
-    Klass = Ember.Object.extend(SortableItemMixin, {
-      $() { return el; }
+    run(() => {
+      subject = MockComponent.create();
+      subject.appendTo('#ember-testing');
     });
-    subject = Klass.create();
+  },
+
+  afterEach() {
+    run(() => {
+      subject.destroy();
+    });
   }
 });
 
 test('isAnimated with transform and duration', function(assert) {
-  el.css({ transition: 'all .25s' });
+  subject.$().css({ transition: 'all .25s' });
 
   assert.equal(subject.get('isAnimated'), true,
     'expected isAnimated to be true');
 });
 
 test('isAnimated with transform but no duration', function(assert) {
-  el.css({ transition: 'all 0s' });
+  subject.$().css({ transition: 'all 0s' });
 
   assert.equal(subject.get('isAnimated'), false,
     'expected isAnimated to be false');
 });
 
 test('isAnimated with duration but no transform', function(assert) {
-  el.css({ transition: 'none' });
+  subject.$().css({ transition: 'none' });
 
   assert.equal(subject.get('isAnimated'), false,
     'expected isAnimated to be false');
 });
+
+test('get y without explicitly setting a value', function(assert) {
+  assert.equal(subject.get('y'), subject.element.offsetTop,
+    'expected y to be element.offsetTop');
+});
+
+test('set y', function(assert) {
+  run(() => {
+    subject.set('y', 50);
+  });
+
+  let transform = getTransform(subject.element);
+
+  assert.equal(transform, 'translateY(50px)',
+    'expected transform to be set');
+  assert.equal(subject.get('y'), 50,
+    'expected y to retain value');
+});
+
+function getTransform(element) {
+  let style = element.style;
+
+  return style.transform ||
+         style.mozTransform ||
+         style.msTransform ||
+         style.oTransform ||
+         style.webkitTransform;
+}
