20deae0b71e83723e1704fe90a7b5a66f6785618
Jamie White <jamie@jgwhite.co.uk>
2015-04-27 08:18:54 +0100

    Remove boilerplate test for sortable-group

--- tests/unit/components/sortable-group-test.js
+++ tests/unit/components/sortable-group-test.js
@@ -5,18 +5,6 @@ import {
 
 moduleForComponent('sortable-group');
 
-test('it renders', function(assert) {
-  assert.expect(2);
-
-  // Creates the component instance
-  var component = this.subject();
-  assert.equal(component._state, 'preRender');
-
-  // Renders the component to the page
-  this.render();
-  assert.equal(component._state, 'inDOM');
-});
-
 test('itemPosition', function(assert) {
   let component = this.subject();
 
