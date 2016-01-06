d3a660eb9cb74632f592764bd8f92e6e14928566
Jamie White <jamie@jgwhite.co.uk>
2016-01-06 10:32:18 +0000

    Set base URL back to ''

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dummy',
     environment: environment,
-    baseURL: '/',
+    baseURL: '',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
