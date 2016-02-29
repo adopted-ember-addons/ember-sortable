3ff61f198601c6f7cf5c8e62bd6eeaaea90454f5
Jamie White <jamie@jgwhite.co.uk>
2016-02-29 07:27:49 +0000

    Improve statemachine

--- addon/mixins/sortable-node.js
+++ addon/mixins/sortable-node.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-const { Mixin, run: { bind } } = Ember;
+const { Mixin } = Ember;
 
 const THRESHOLD = 50; // ms
 
@@ -45,7 +45,7 @@ export default Mixin.create({
   _sortableStart({ originalEvent }) {
     let machine = new SortableStateMachine();
 
-    machine.onUpdate = bind(this, '_sortableUpdate');
+    machine.onUpdate = machine => this._sortableUpdate(machine);
 
     machine.start(originalEvent);
   },
@@ -83,6 +83,7 @@ class SortableStateMachine {
     this.oy = 0;
     this.dx = 0;
     this.dy = 0;
+    this.listeners = [];
   }
 
   /**
@@ -90,94 +91,176 @@ class SortableStateMachine {
    * @param {UIEvent} event
    */
   start(event) {
+    switch (this.state) {
+      case 'default':
+        this.defaultStart(event);
+    }
+  }
+
+  /**
+   * @method defaultStart
+   * @param {UIEvent} event
+   */
+  defaultStart(event) {
     this.state = 'waiting';
     this.ot = Date.now();
-    this.ox = event.pageX;
-    this.oy = event.pageY;
 
-    this._move = bind(this, 'move');
-    this._stop = bind(this, 'stop');
+    let touch = event.changedTouches && event.changedTouches[0];
 
-    on('mousemove', this._move);
-    on('touchmove', this._move);
-    on('mouseup', this._stop);
-    on('touchend', this._stop);
+    if (touch) {
+      let id = touch.identifier;
+
+      this.ox = touch.pageX;
+      this.oy = touch.pageY;
+
+      this.on('touchmove', touchbind(id, (e, t)  => this.move(e, t)));
+      this.on('touchend', touchbind(id, () => this.stop()));
+    } else {
+      this.ox = event.pageX;
+      this.oy = event.pageY;
+
+      this.on('mousemove', e => this.move(e));
+      this.on('mouseup', e => this.stop(e));
+    }
 
     this.onUpdate(this);
   }
+
 
   /**
    * @method move
    * @param {UIEvent} event
+   * @param {Touch} [touch]
    */
-  move(event) {
+  move(event, touch) {
     switch (this.state) {
       case 'waiting':
-        let isTouch = event instanceof window.TouchEvent;
-        let isTooFast = Date.now() - this.ot < THRESHOLD;
-
-        if (isTouch && isTooFast) {
-          this.state = 'swiping';
-          this.stop();
-        } else {
-          this.state = 'dragging';
-          this.move(event);
-        }
-
+        this.waitingMove(event, touch);
         break;
       case 'dragging':
-        event.preventDefault();
-
-        this.dx = event.pageX - this.ox;
-        this.dy = event.pageY - this.oy;
-
-        this.onUpdate(this);
-
+        this.draggingMove(event, touch);
         break;
     }
+  }
+
+  /**
+   * @method waitingMove
+   * @param {UIEvent} event
+   * @param {Touch|null} [touch]
+   */
+  waitingMove(event, touch) {
+    let isTooFast = touch && (Date.now() - this.ot < THRESHOLD);
+
+    if (isTooFast) {
+      this.state = 'swiping';
+      this.onUpdate(this);
+      this.destroy();
+    } else {
+      this.state = 'dragging';
+      this.move(event);
+    }
+  }
+
+  /**
+   * @method draggingMove
+   * @param {UIEvent} event
+   * @param {Touch} [touch]
+   */
+  draggingMove(event, touch) {
+    event.preventDefault();
+
+    this.dx = (touch || event).pageX - this.ox;
+    this.dy = (touch || event).pageY - this.oy;
+
+    this.onUpdate(this);
   }
 
   /**
    * @method stop
-   * @param {null|UIEvent} event
    */
   stop() {
-    off('mousemove', this._move);
-    off('touchmove', this._move);
-    off('mouseup', this._stop);
-    off('touchend', this._stop);
-
     switch (this.state) {
       case 'waiting':
-        this.state = 'clicked';
+        this.waitingStop();
         break;
       case 'dragging':
-        this.state = 'dropped';
-        preventClick();
+        this.draggingStop();
         break;
     }
-
-    this.onUpdate(this);
-
-    delete this.onUpdate;
   }
 
   /**
-   * @method onUpdate
-   * @param {SortableStateMachine} machine
+   * @method waitingStop
    */
-  onUpdate() {}
+  waitingStop() {
+    this.state = 'clicked';
+    this.onUpdate(this);
+    this.destroy();
+  }
+
+  /**
+   * @method draggingStop
+   */
+  draggingStop() {
+    this.state = 'dropped';
+    preventNextClick();
+    this.onUpdate(this);
+    this.destroy();
+  }
+
+  /**
+   * @method on
+   * @param {String} event
+   * @param {Function} callback
+   * @return {Object}
+   */
+  on(event, callback) {
+    window.addEventListener(event, callback, true);
+
+    this.listeners.push({
+      remove() {
+        window.removeEventListener(event, callback, true);
+      }
+    });
+  }
+
+  /**
+   * @method destroy
+   */
+  destroy() {
+    this.listeners.forEach(l => l.remove());
+    delete this.onUpdate;
+  }
+
 }
 
-function on(event, callback) {
-  window.addEventListener(event, callback, true);
+/**
+ * @method touchbind
+ * @param {Number} id
+ * @param {Function} callback
+ * @return {Function}
+ */
+function touchbind(id, callback) {
+  return event => {
+    let touch = find(event.changedTouches, t => t.identifier === id);
+    if (touch) { callback(event, touch); }
+  };
 }
 
-function off(event, callback) {
-  window.removeEventListener(event, callback, true);
+/**
+ * @method find
+ * @param {Array} list
+ * @param {Function} callback
+ * @return {Any}
+ */
+function find(list, callback) {
+  return Array.prototype.find.call(list, callback);
 }
 
-function preventClick() {
+/**
+ * @method preventNextClick
+ */
+function preventNextClick() {
   let noclick = event => {
     event.stopPropagation();
     window.removeEventListener('click', noclick, true);
