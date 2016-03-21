7b27a5f3d381ab1da67c1d6eb34a9cb3a5a4c11a
Jamie White <jamie@jgwhite.co.uk>
2016-03-21 17:54:17 +0100

    Improve Ember.A usage

--- tests/dummy/app/routes/nested.js
+++ tests/dummy/app/routes/nested.js
@@ -5,56 +5,56 @@ export default Route.extend({
   model() {
     return {
       label: 'Root',
-      children: A([{
+      children: [{
         label: '1',
-        children: A([{
+        children: [{
           label: '1.1',
-          children: A([{
+          children: [{
             label: '1.1.1'
           },{
             label: '1.1.2'
           },{
             label: '1.1.3'
-          }])
+          }]
         }, {
           label: '1.2',
-          children: A([{
+          children: [{
             label: '1.2.1'
           },{
             label: '1.2.2'
           },{
             label: '1.2.3'
-          }])
-        }])
+          }]
+        }]
       }, {
         label: '2',
-        children: A([{
+        children: [{
           label: '2.1',
-          children: A([{
+          children: [{
             label: '2.1.1'
           },{
             label: '2.1.2'
           },{
             label: '2.1.3'
-          }])
+          }]
         }, {
           label: '2.2',
-          children: A([{
+          children: [{
             label: '2.2.1'
           },{
             label: '2.2.2'
           },{
             label: '2.2.3'
-          }])
-        }])
-      }])
+          }]
+        }]
+      }]
     };
   },
 
   actions: {
     moveNode(node, { oldParent, newParent, position }) {
-      oldParent.children.removeObject(node);
-      newParent.children.insertAt(position, node);
+      A(oldParent.children).removeObject(node);
+      A(newParent.children).insertAt(position, node);
     }
   }
 });
