27253f199c751cb376f3c15652c437e755f65901
Jamie White <jamie@jgwhite.co.uk>
2016-03-21 12:19:57 +0100

    Use observable arrays in nested route

--- tests/dummy/app/routes/nested.js
+++ tests/dummy/app/routes/nested.js
@@ -1,63 +1,60 @@
 import Route from 'ember-route';
+import { A } from 'ember-array/utils';
 
 export default Route.extend({
   model() {
     return {
       label: 'Root',
-      children: [{
+      children: A([{
         label: '1',
-        children: [{
+        children: A([{
           label: '1.1',
-          children: [{
+          children: A([{
             label: '1.1.1'
           },{
             label: '1.1.2'
           },{
             label: '1.1.3'
-          }]
+          }])
         }, {
           label: '1.2',
-          children: [{
+          children: A([{
             label: '1.2.1'
           },{
             label: '1.2.2'
           },{
             label: '1.2.3'
-          }]
-        }]
+          }])
+        }])
       }, {
         label: '2',
-        children: [{
+        children: A([{
           label: '2.1',
-          children: [{
+          children: A([{
             label: '2.1.1'
           },{
             label: '2.1.2'
           },{
             label: '2.1.3'
-          }]
+          }])
         }, {
           label: '2.2',
-          children: [{
+          children: A([{
             label: '2.2.1'
           },{
             label: '2.2.2'
           },{
             label: '2.2.3'
-          }]
-        }]
-      }]
+          }])
+        }])
+      }])
     };
   },
 
   actions: {
     moveNode(node, { oldParent, newParent, position }) {
-      let index = oldParent.children.indexOf(node);
-
-      oldParent.children.splice(index, 1);
-      newParent.children.splice(position, 0, node);
-
-      this.get('controller').notifyPropertyChange('model');
+      oldParent.children.removeObject(node);
+      newParent.children.insertAt(position, node);
     }
   }
 });
