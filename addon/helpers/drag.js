a5be84f36f676a0297864fd3cb01cc2c2709d1c4
Dray Lacy <dray@envylabs.com>
2016-10-22 15:51:23 -0400

    Replace outerHeight with clientHeight for scaling
    This seems more reliable for getting the original (pre-transform) height
    of the element.

--- addon/helpers/drag.js
+++ addon/helpers/drag.js
@@ -52,8 +52,9 @@ export function drag(app, mode, itemSelector, offsetFn, callbacks = {}) {
     let item = findWithAssert(itemSelector);
     let itemOffset = item.offset();
     let offset = offsetFn();
-    let rect = item.get(0).getBoundingClientRect();
-    let scale = item.outerHeight() / (rect.bottom - rect.top);
+    let itemElement = item.get(0);
+    let rect = itemElement.getBoundingClientRect();
+    let scale = itemElement.clientHeight / (rect.bottom - rect.top);
     let targetX = itemOffset.left + offset.dx * scale;
     let targetY = itemOffset.top + offset.dy * scale;
 
