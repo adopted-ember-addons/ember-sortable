9b03b52d02197477beb0eda5f751641d889d9377
Tim Evans <tevans@movableink.com>
2016-10-27 13:17:04 -0400

    Autoscroll while dragging sortable-items (#104)
    * scroll the container the list is in when it reaches the edges
    
    this builds in functionality to scroll the container when a sortable-item
    reaches the bounding edge of its parent scroll container.
    
    * make the max scroll speed configurable (but with a sensible default)
    
    * get smooth dragging working in the document window

---
+++ addon/system/scroll-parent.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+const { $ } = Ember;
+
+export default function ($element) {
+  let position = $element.css('position');
+  let excludeStaticParent = position === 'absolute';
+  let $scrollParent = $element.parents().filter(function () {
+    let $parent = $(this);
+    if (excludeStaticParent && $parent.css('position') === 'static') {
+      return false;
+    }
+    let { overflow, overflowX, overflowY } = $parent.css(['overflow', 'overflowX', 'overflowY']);
+    return /(auto|scroll)/.test(overflow + overflowX + overflowY);
+  }).eq(0);
+
+  if ($scrollParent.length === 0) {
+    $scrollParent = $(document);
+  }
+  return position === 'fixed' || $scrollParent;
+}
