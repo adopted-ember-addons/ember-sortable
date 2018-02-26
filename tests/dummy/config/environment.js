34121b6a4ce49fdadf239cf5d8432c1b1f720297
Jamie White <jamie@jgwhite.co.uk>
2018-02-26 14:10:37 +0000

    Make rootURL: '' and location: 'hash'
    Closes #173

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -4,8 +4,8 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'dummy',
     environment,
-    rootURL: '/',
-    locationType: 'auto',
+    rootURL: '',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
