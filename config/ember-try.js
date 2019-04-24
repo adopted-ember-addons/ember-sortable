85faf53695a2589c8872bfb4aeae7fc67195bdf6
Matthew Beale <matt.beale@addepar.com>
2019-04-24 12:14:55 -0400

    Add Ember 2.8, 2.12 to testing. Test Node 8

--- config/ember-try.js
+++ config/ember-try.js
@@ -12,6 +12,34 @@ module.exports = function() {
       useYarn: true,
       scenarios: [
         {
+          name: 'ember-lts-2.8',
+          env: {},
+          bower: {
+            dependencies: {
+              ember: 'components/ember#lts-2-8',
+            },
+            resolutions: {
+              ember: 'lts-2-8',
+            },
+          },
+          npm: {
+            devDependencies: {
+              '@ember/jquery': '^0.5.1',
+              'ember-source': null,
+            }
+          },
+        },
+        {
+          name: 'ember-lts-2.12',
+          env: {},
+          npm: {
+            devDependencies: {
+              '@ember/jquery': '^0.5.1',
+              'ember-source': '~2.12.0'
+            }
+          }
+        },
+        {
           name: 'ember-lts-2.18',
           env: {
             EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
