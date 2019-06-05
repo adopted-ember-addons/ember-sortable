959c7358eb2952049406ea0247b5336a4d7b7539
Nathaniel Furniss <nlfurniss@gmail.com>
2019-06-04 22:31:59 -0700

    First pass at upgrading to new QUnit syntax

--- tests/integration/components/sortable-group-test.js
+++ tests/integration/components/sortable-group-test.js
@@ -1,46 +1,43 @@
-import $ from 'jquery';
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-import { triggerEvent } from '@ember/test-helpers';
+import { render, find, triggerEvent } from '@ember/test-helpers';
 
-moduleForComponent('sortable-group', 'Integration | Component | sortable group', {
-  integration: true
-});
+module('Integration | Component | sortable group', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('distance attribute prevents the drag before the specified value', async function(assert) {
-  this.render(hbs`
-    {{#sortable-group as |group|}}
-      {{#group.item distance=15 model=1 id="dummy-sortable-item"}}
-        {{item}}
-      {{/group.item}}
-    {{/sortable-group}}
-  `);
+  test('distance attribute prevents the drag before the specified value', async function(assert) {
+    await render(hbs`
+      {{#sortable-group as |group|}}
+        {{#sortable-item distance=15 model=1 group=group id="dummy-sortable-item"}}
+          {{item}}
+        {{/sortable-item}}
+      {{/sortable-group}}
+    `);
 
-  let item = $('#dummy-sortable-item');
-  let itemElem = item[0];
-  let itemOffset = item.offset();
+    let itemElem = find('#dummy-sortable-item');
+    let itemOffset = itemElem.getBoundingClientRect();
 
-  await triggerEvent(itemElem, 'mousedown', { clientX: itemOffset.left, clientY: itemOffset.top, which: 1 });
-  await triggerEvent(itemElem, 'mousemove', { clientX: itemOffset.left, clientY: itemOffset.top, which: 1 });
-  await triggerEvent(itemElem, 'mousemove', { clientX: itemOffset.left, clientY: itemOffset.top + 5, which: 1 });
+    await triggerEvent(itemElem, 'mousedown', { clientX: itemOffset.left, clientY: itemOffset.top, which: 1 });
+    await triggerEvent(itemElem, 'mousemove', { clientX: itemOffset.left, clientY: itemOffset.top, which: 1 });
+    await triggerEvent(itemElem, 'mousemove', { clientX: itemOffset.left, clientY: itemOffset.top + 5, which: 1 });
 
-  assert.notOk(item.hasClass('is-dragging'), 'does not start dragging if the drag distance is less than the passed one');
+    assert.dom(itemElem).doesNotHaveClass('is-dragging', 'does not start dragging if the drag distance is less than the passed one');
 
-  await triggerEvent(itemElem, 'mousemove', { clientX: itemOffset.left, clientY: itemOffset.top + 20, which: 1 });
+    await triggerEvent(itemElem, 'mousemove', { clientX: itemOffset.left, clientY: itemOffset.top + 20, which: 1 });
 
-  assert.ok(item.hasClass('is-dragging'), 'starts dragging if the drag distance is more than the passed one');
-});
+    assert.dom(itemElem).hasClass('is-dragging', 'starts dragging if the drag distance is more than the passed one');
+  });
 
-test('sortable-items have tabindexes for accessibility', function (assert) {
-  this.render(hbs`
-    {{#sortable-group as |group|}}
-      {{#group.item tabindex=0 model=1 id="dummy-sortable-item"}}
-        sort me
-      {{/group.item}}
-    {{/sortable-group}}
-  `);
+  test('sortable-items have tabindexes for accessibility', async function(assert) {
+    await render(hbs`
+      {{#sortable-group as |group|}}
+        {{#sortable-item tabindex=0 model=1 id="dummy-sortable-item"}}
+          sort me
+        {{/sortable-item}}
+      {{/sortable-group}}
+    `);
 
-  let item = this.$('#dummy-sortable-item');
-
-  assert.equal(item.attr('tabindex'), 0, 'sortable-items have tabindexes');
+    assert.dom('#dummy-sortable-item').hasAttribute('tabindex', '0', 'sortable-items have tabindexes');
+  });
 });
