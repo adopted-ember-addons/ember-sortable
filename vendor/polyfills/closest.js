8b8df6e7ed835584f6870c9c96343e67edc12240
Yicheng Gong <yigong@yigong-mn4.linkedin.biz>
2019-10-05 17:06:29 -0700

    Removing matches polyfill from closest polyfill

--- vendor/polyfills/closest.js
+++ vendor/polyfills/closest.js
@@ -2,32 +2,12 @@
 // https://github.com/jonathantneal/closest
 if (typeof window !== 'undefined' && typeof document !== 'undefined') {
   (function(ElementProto) {
-    if (typeof ElementProto.matches !== 'function') {
-      ElementProto.matches =
-        ElementProto.msMatchesSelector ||
-        ElementProto.mozMatchesSelector ||
-        ElementProto.webkitMatchesSelector ||
-        function matches(selector) {
-          var element = this;
-          var elements = (
-            element.document || element.ownerDocument
-          ).querySelectorAll(selector);
-          var index = 0;
-
-          while (elements[index] && elements[index] !== element) {
-            ++index;
-          }
-
-          return Boolean(elements[index]);
-        };
-    }
-
     if (typeof ElementProto.closest !== 'function') {
       ElementProto.closest = function closest(selector) {
         var element = this;
 
         while (element && element.nodeType === 1) {
-          if (element.matches(selector)) {
+          if (element.msMatchesSelector(selector)) {
             return element;
           }
 
