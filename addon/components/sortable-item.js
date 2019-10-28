f907b9fcc5af07354e28ae8c1bb08548cc3be956
Yicheng Gong <ygongdev@gmail.com>
2019-10-27 20:11:26 -0700

    Adding API for using an annoucement config for different keyboard operations

--- addon/components/sortable-item.js
+++ addon/components/sortable-item.js
@@ -16,6 +16,7 @@ const sortableItemWaiter = buildWaiter("sortable-item-waiter");
 
 export default Component.extend({
   layout,
+  tagName: 'li',
   classNames: ['sortable-item'],
   classNameBindings: ['isDragging', 'isDropping'],
 
