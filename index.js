131a8fcbdbc49d0cdacfdda376249f06b0e36950
Yicheng (Jerry) Gong <ygongdev@gmail.com>
2019-11-18 22:22:59 -0800

    Attempt to fix vendor on consuming app again (#330)

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
-const path = require('path');
 
 module.exports = {
   name: require('./package').name,
 
   included(app) {
-    app.import(path.join(this.treePaths.vendor, 'polyfills', 'closest.js'), { prepend: true})
+    this._super.included(...arguments);
+
+    app.import('vendor/polyfills/closest.js', { prepend: true});
   }
 };
