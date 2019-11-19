76566bdc58f1dff3068b2f7e828abb62d11251d9
Yicheng Gong <ygongdev@gmail.com>
2019-11-19 11:51:21 -0800

    Mitigate vendor build error

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 'use strict';
 
+const fs = require('fs');
+
 module.exports = {
   name: require('./package').name,
 
   included(app) {
     this._super.included.apply(this, arguments);
 
-    app.import('vendor/polyfills/closest.js', { prepend: true});
+    // Sometimes, consuming app production build can't find the vendor, so it'll break the build with a sourceMapConcat error.
+    // So guarding this with a if polyfill exists.
+    if (fs.existsSync('vendor/polyfills/closest.js')) {
+      app.import('vendor/polyfills/closest.js', { prepend: true});
+    }
   }
 };
