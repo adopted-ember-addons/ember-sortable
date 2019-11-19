e040e26eabd8cb9c39014b5001d30ffe00b1e6d1
Yicheng Gong <ygongdev@gmail.com>
2019-11-18 23:00:22 -0800

    Attempt to fix vendor on consuming again again haha

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = {
   name: require('./package').name,
 
   included(app) {
-    this._super.included(...arguments);
+    this._super.included.apply(this, arguments);
 
     app.import('vendor/polyfills/closest.js', { prepend: true});
   }
