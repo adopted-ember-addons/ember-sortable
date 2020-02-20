81320bdf255bfa2a73f167fdd17c7240942ec8b2
Steve <Steve.Hummingbird@yandex.com>
2020-02-20 19:06:37 +0100

    fixes handle when using modifiers (#349)
    * fix check if clicked element is modifier
    
    * fix initialization of handleElement
    
    * add test to assert that dragging an element only works by selecting the handle (when it is present)

--- addon/modifiers/sortable-item.js
+++ addon/modifiers/sortable-item.js
@@ -266,10 +266,7 @@ export default class SortableItemModifier extends Modifier {
       return;
     }
 
-    let handle = this.handle;
-
-    // todo what does this do? why do you have to null handle
-    if (!this.handleElement && !startEvent.target.closest(handle)) {
+    if (this.handleElement && !startEvent.target.closest(this.handle)) {
       return;
     }
 
@@ -743,12 +740,11 @@ export default class SortableItemModifier extends Modifier {
     // Instead of using `event.preventDefault()` in the 'primeDrag' event,
     // (doesn't work in Chrome 56), we set touch-action: none as a workaround.
     this.handleElement = this.element.querySelector(this.handle);
-
-    if (!this.handleElement) {
-      this.handleElement = this.element;
+    if (this.handleElement) {
+      this.handleElement.style['touch-action'] = 'none';
+    } else {
+      this.element.style['touch-action'] = 'none';
     }
-
-    this.handleElement.style['touch-action'] = 'none';
   }
 
   didInstall() {
