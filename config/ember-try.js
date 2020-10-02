a2ad1bb35e73375a85381eb9167c64282d369c95
Yicheng Gong <ygongdev@gmail.com>
2020-10-01 23:51:39 -0700

    Searching for ember source version with bad tests

--- config/ember-try.js
+++ config/ember-try.js
@@ -12,26 +12,26 @@ module.exports = function() {
       useYarn: true,
       scenarios: [
         {
-          name: 'ember-lts-3.8',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.8.0'
-            }
-          }
-        },
-        {
-          name: 'ember-lts-3.12',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.12.0'
-            }
-          }
-        },
-        {
           name: 'ember-lts-3.15',
           npm: {
             devDependencies: {
               'ember-source': '~3.15.0'
+            }
+          }
+        },
+        {
+          name: 'ember-lts-3.16',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.16.0'
+            }
+          }
+        },
+        {
+          name: 'ember-lts-3.17',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.17.0'
             }
           }
         },
