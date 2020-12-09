d4c5aca60841f0de41a20a04924fcf9bbec2d940
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-12-09 12:22:35 -0500

    apply ember-cli-update, observe lots of new lint violations (footguns / incorrect code mostly)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
+'use strict';
+
 module.exports = {
   root: true,
   parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
+    ecmaFeatures: {
+      legacyDecorators: true
+    }
   },
   plugins: [
     'ember'
@@ -48,9 +53,7 @@ module.exports = {
         node: true
       },
       plugins: ['node'],
-      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
-        // add your custom rules and overrides for node files here
-      })
+      extends: ['plugin:node/recommended']
     }
   ]
 };
