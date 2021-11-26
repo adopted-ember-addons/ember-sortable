40e6f61504ba9e45aa4e82d9127b19c53ca7b2cf
Scott Newcomer <snewcomer24@gmail.com>
2021-11-26 17:09:16 -0600

    General updates to dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
     {
       files: [
         '.eslintrc.js',
+        '.prettierrc.js',
         '.template-lintrc.js',
         'ember-cli-build.js',
         'index.js',
--- tests/dummy/app/components/item-presenter/component.js
+++ tests/dummy/app/components/item-presenter/component.js
@@ -1,9 +1,7 @@
 import Component from '@ember/component';
 
-export default Component.extend({
-  item: null,
-
-  click: function(){
+export default class ItemPresenter extends Component {
+  click() {
     console.log('Item clicked'); // eslint-disable-line no-console
   }
-});
+}
