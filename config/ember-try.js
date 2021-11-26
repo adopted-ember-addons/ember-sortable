41dd4a5fce69b65988d370c63bb1d1029fc312ca
Scott Newcomer <snewcomer24@gmail.com>
2021-11-26 09:46:48 -0600

    rm old testing scenarios

--- config/ember-try.js
+++ config/ember-try.js
@@ -12,31 +12,6 @@ module.exports = function() {
       useYarn: true,
       scenarios: [
         {
-          name: 'ember-lts-3.8',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.8.0',
-              'ember-decorators-polyfill': '^1.1.1',
-            }
-          }
-        },
-        {
-          name: 'ember-lts-3.12',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.12.0'
-            }
-          }
-        },
-        {
-          name: 'ember-lts-3.16',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.16.0'
-            }
-          }
-        },
-        {
           name: 'ember-lts-3.20',
           npm: {
             devDependencies: {
