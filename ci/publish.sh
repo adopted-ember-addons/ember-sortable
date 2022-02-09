681d65cbb2880b77ae061b5b5a647b733c7009bc
Daniel Genis <d.genis@gynzy.com>
2022-02-09 11:52:20 +0100

    [POSID-135-fix-publish] fix publish script for tagging actual releases

--- ci/publish.sh
+++ ci/publish.sh
@@ -3,13 +3,13 @@
 set -eo pipefail
 
 VERSION=`yarn version --non-interactive 2>/dev/null | grep 'Current version' | awk '{ print $4 }'`
-if [[ ! -z "{$PR_NUMBER}" ]]; then
-  echo "Setting tag/version for pr build."
+if [[ ! -z "${PR_NUMBER}" ]]; then
+	echo "Setting tag/version for pr build."
 	TAG=pr-$PR_NUMBER
 	PUBLISHVERSION="$VERSION-pr$PR_NUMBER.$GITHUB_RUN_NUMBER"
-elif [[ "{$GITHUB_REF_TYPE}" == "tag" ]]; then
-	if [[ "{$GITHUB_REF_NAME}" != "{$VERSION}" ]]; then
-	  echo "Tag version does not match package version. They should mathc. Exiting"
+elif [[ "${GITHUB_REF_TYPE}" == "tag" ]]; then
+	if [[ "${GITHUB_REF_NAME}" != "${VERSION}" ]]; then
+	  echo "Tag version does not match package version. They should match. Exiting"
 		exit 1
 	fi
 	echo "Setting tag/version for release/tag build."
