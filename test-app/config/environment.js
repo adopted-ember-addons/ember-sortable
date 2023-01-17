a254bdc9ed9860cb1dc27280e4441b3644be04ec
Leo Euclides <leoeuclidss@gmail.com>
2023-01-17 20:53:21 -0300

    Fix lint and test errors

--- test-app/config/environment.js
+++ test-app/config/environment.js
@@ -20,7 +20,7 @@ module.exports = function (environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    }
+    },
   };
 
   if (environment === 'development') {
