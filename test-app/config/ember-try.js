324e0e1a1df133bfbcb9030ed98f17ba6e378b11
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-12-19 17:59:50 -0500

    Test against LTS 4.12 and 5.4

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -32,6 +32,22 @@ module.exports = async function () {
         },
       },
       {
+        name: 'ember-lts-4.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.12.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-5.4',
+        npm: {
+          devDependencies: {
+            'ember-source': '~5.4.0',
+          },
+        },
+      },
+      {
         name: 'ember-modifier-4',
         npm: {
           devDependencies: {
