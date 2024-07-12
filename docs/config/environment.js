a4b0699a2c18d13b273a5ca2fb49bc879986f17c
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-07-12 10:59:52 -0400

    Update rootURL for gh-pages

--- docs/config/environment.js
+++ docs/config/environment.js
@@ -62,6 +62,7 @@ module.exports = function (environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
+    ENV.rootURL = '/ember-sortable/';
   }
 
   return ENV;
