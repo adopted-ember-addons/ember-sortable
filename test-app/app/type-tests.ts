43453b8ed7538af5ef21a0720daf24e5039e0b1c
Charles Fries <charliefries@icloud.com>
2024-07-16 09:38:42 -0700

    Install Glint (WIP)

---
+++ test-app/app/type-tests.ts
@@ -0,0 +1,7 @@
+import sortableGroup from 'ember-sortable/modifiers/sortable-group';
+import sortableHandle from 'ember-sortable/modifiers/sortable-handle';
+import sortableItem from 'ember-sortable/modifiers/sortable-item';
+
+sortableGroup;
+sortableHandle;
+sortableItem;
