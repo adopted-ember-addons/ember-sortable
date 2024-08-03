5d5826874765ed3b3e1413596a051642e36ba557
Markus Sanin <markus.sanin@zeppelin-group.com>
2024-08-03 10:44:41 +0200

    Add test for `transitionDuration` when only one item is present which will be moved

---
+++ test-app/tests/integration/modifiers/sortable-item-test.js
@@ -0,0 +1,36 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { find, render } from '@ember/test-helpers';
+import { set } from '@ember/object';
+import { drag } from 'ember-sortable/test-support';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Modifier | sortable-item', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('Drag works with one item', async function (assert) {
+    this.items = ['Uno'];
+
+    this.update = (items) => {
+      set(this, 'items', items);
+    };
+
+    await render(hbs`
+      <ol id="test-list" {{sortable-group onChange=this.update}}>
+        {{#each this.items as |item|}}
+          <li data-test-item {{sortable-item model=item}}>{{item}}</li>
+        {{/each}}
+      </ol>
+    `);
+
+    await drag('mouse', '[data-test-item]', () => {
+      return { dy: 10 };
+    });
+
+    assert.equal(contents('#test-list'), 'Uno');
+  });
+
+  function contents(selector) {
+    return find(selector).textContent.replace(/⇕/g, '').replace(/\s+/g, ' ').replace(/^\s+/, '').replace(/\s+$/, '');
+  }
+});
