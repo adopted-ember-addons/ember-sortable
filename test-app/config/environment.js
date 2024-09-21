d370c9ed79e63424201e50d9958f35d69b3156a1
Markus Sanin <markus.sanin@zeppelin-group.com>
2024-09-21 07:56:22 +0200

    Disable prototype extension

--- test-app/config/environment.js
+++ test-app/config/environment.js
@@ -7,13 +7,10 @@ module.exports = function (environment) {
     rootURL: '/',
     locationType: 'history',
     EmberENV: {
+      EXTEND_PROTOTYPES: false,
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
-      },
-      EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
-        Date: false,
       },
     },
 
